c19ee2f85876fa2fad33364120dd59443979f702
dima-leveret <dimazayac@gmail.com>
2022-05-27 16:23:40 +0100

    connected to the cloudinary, display all images, added env variables

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,10 +1,79 @@
 import Headcomponent from "../../components/HeadComponent";
+import { useState } from "react";
+import Image from "next/image";
 
-export default function Gallery() {
+import { searchAllImages, mapImageResources } from "../../lib/cloudinary";
+
+export default function Gallery({ images: defaultImages, nextCursor: defaultNextCursor }) {
+  const [images, setImages] = useState(defaultImages);
+  const [nextCursor, setNextCursor] = useState(defaultNextCursor);
+
+  console.log('images => ', images);
+  console.log('nextCursor => ', nextCursor);
+
+  const handleLoadMoreImages = async (e) => {
+    e.preventDefault()
+    const results = await fetch('/api/searchAllImages', {
+      method: "POST",
+      body: JSON.stringify({
+        nextCursor
+      })
+    }).then(r => r.json());
+
+    const { resources, next_cursor: updatedNextCursor } = results;
+
+    const images = mapImageResources(resources);
+
+    setImages(prev => {
+      return [
+        ...prev,
+        ...images
+      ]
+    });
+
+    setNextCursor(updatedNextCursor);
+  }
+
   return (
     <div>
       <Headcomponent title="Geleria" />
       <h4>Zdięcia samochodów/klientów</h4>
+      <div>
+        {
+          images.map(image => {
+            return <img
+              width={300}
+              height={300}
+              key={image.asset_id}
+              src={image.secure_url}
+            />
+          })
+        }
+      </div>
+      {
+        nextCursor
+        ?
+        <button onClick={handleLoadMoreImages}>Pokaż wicej zdjęć</button>
+        :
+        <span> To są wszystkie zdjęcia które obecnie mamy 😃</span>
+      }
     </div>
   );
+}
+
+
+export async function getStaticProps() {
+
+  const results = await searchAllImages()
+
+  const { resources, next_cursor: nextCursor } = results;
+
+  const images = mapImageResources(resources)
+
+  return {
+    props: {
+      images,
+      nextCursor
+    }
+  }
 }
--- pages/price-list/index.js
+++ pages/price-list/index.js
@@ -1,10 +1,33 @@
 import Headcomponent from "../../components/HeadComponent";
 
-export default function Gallery() {
+import { searchAllImages, mapImageResources, getFolders } from "../../lib/cloudinary";
+
+export default function PriceList({ images, folders }) {
+  console.log('images => ', images);
+  console.log('folders => ', folders);
   return (
     <div>
       <Headcomponent title="Cennik usług" />
       <h4>Cennik usług</h4>
     </div>
   );
+}
+
+export async function getStaticProps() {
+
+  const results = await searchAllImages()
+
+  const { resources } = results;
+
+  const images = mapImageResources(resources);
+
+  const { folders } = await getFolders();
+  console.log("folder =>", folders);
+
+  return {
+    props: {
+      images,
+      folders
+    }
+  }
 }
